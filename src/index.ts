3919e345e9017ec22b88cc2d0dd5dc64699ee616
Jimy <jimyhoulbrook@gmail.com>
2023-11-16 18:36:07 +0000

    bouncing text

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,65 @@
-import { Application } from 'pixi.js';
+/** Pixi application boilerplate
+ *  Jimy Houlbrook
+ *  16/11/23
+ */
 
-export default class Main{
-    private app : Application;
+import * as PIXI from 'pixi.js';
+
+export default class Application {
+
+    private app : PIXI.Application;
+
+    private velocity;
 
     constructor(){
-        this.app = new Application<HTMLCanvasElement>({
-            height: 500,
-            width: 500,
+        this.app = new PIXI.Application<HTMLCanvasElement>({
+            height: 720,
+            width: 1080,
+            backgroundColor: 0x000000,
             hello: true,
-            view: <HTMLCanvasElement>document.getElementById("pixi")
+            view: <HTMLCanvasElement>document.getElementById('pixi')
         });
+
+        this.velocity = {
+            x: 1,
+            y: 1
+        };
+
+        const textStyle : PIXI.TextStyle = new PIXI.TextStyle({
+            fill: "#c09947",
+            fontFamily: 'Arial',
+            fontSize: 120,
+            align: 'left',
+            lineJoin: "round",
+            stroke: "#694329",
+            strokeThickness: 20              
+        });
+
+
+        const text : PIXI.Text = new PIXI.Text("Bounce", textStyle);
+        text.anchor.set(0.5);
+        text.position.set(this.app.view.width / 2, this.app.view.height / 2);
+        this.app.stage.addChild(text);
+
+        this.app.ticker.add( () => this.delta(text));
+    }
+
+    delta(text: PIXI.Text){
+        text.x += this.velocity.x;
+        text.y += this.velocity.y;
+
+        if(
+            text.x + text.width / 2 >= this.app.view.width ||
+            text.x - text.width / 2 <= 0
+        ){
+            this.velocity.x *= -1;
+        } else if (
+            text.y + text.height / 2 >= this.app.view.height ||
+            text.y - text.height / 2 <= 0
+        ){
+            this.velocity.y *= -1
+        }
     }
 }
 
-document.addEventListener('DOMContentLoaded', () => new Main);
+window.addEventListener('DOMContentLoaded', () => new Application);
